f6c3c28b7977682861aecff4abf24c97a81dcf5e
James <Thylinguistics@gmail.com>
2021-04-04 01:32:00 -0700

    ...

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,10 +2,24 @@
     <div class="container">
         search component 
         <div class="randomPhotosGrid mt-5">
-             <img :key="item.id" v-for="item in test" :src="item.urls.regular"  class="grid-item" :alt="item.alt_description"/> 
-       
+         <img class="grid-item" :key="item.id" v-for="item in test" :src="item.urls.regular"   :alt="item.alt_description"/> 
        </div>
+
+      
+
+
     </div>
+     <!--
+     <div class="content">
+        <div class="doc">
+            <img src="https://source.unsplash.com/random" alt="" />
+            <div class="links">
+            <a href=""><i class="fa fa-heart"></i><span></span></a>
+            <a href=""><i class="fa fa-comment"></i><span></span></a>
+            </div>
+        </div>
+        </div>
+        !-->
 </template>
 
 
@@ -45,11 +59,73 @@ li{
     gap: 15px 10px;
 }
 
-.grid-item{
+.grid-item {
     width: 350px;
     height: auto;
 }
+.grid-item:hover{
+    transition:.3s;
+    background-color:rgba(0,0,0,5);
+    opacity: 0.8;
 
+}
+
+
+/* insta */
+.doc{
+    position:absolute;
+  left:50%;
+  top:50%;
+  transform:translate(-50%,-50%);
+  font-size:0;
+}
+.doc::before{
+    content:"";
+    transition:.3s;
+    position:absolute;
+    width:100%;
+    height:100%;
+    background-color:rgba(0,0,0,0);
+  }
+.doc .links{
+  text-align:center;
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  overflow:hidden;
+  opacity:0;
+  transition:.2s;
+}
+.doc .links i{
+  font-size:35px;
+  margin:0 auto;
+  position:relative;
+  padding:15px;
+
+}
+.doc a{
+  color:#FFF;
+  position:relative;
+  text-decoration:none;
+}
+.doc span{
+    font-size:16px;
+}
+
+.doc:hover{
+      opacity:1;
+      background-color:rgba(0,0,0,.5);
+}
+
+.doc::before{
+      opacity:1;
+      background-color:rgba(0,0,0,.5);
+}
+
+    .links{
+      opacity:1;
+    }
 
 
     
