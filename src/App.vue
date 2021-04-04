73a4171147dc49e6e80367868ae9576bce0515e9
James <Thylinguistics@gmail.com>
2021-04-04 16:09:19 -0700

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,22 @@
 <template>
    <SearchBar @term-submitted="termSubmitted" />
-  
    <router-view />
 </template>
 
 <script>
 import SearchBar from './components/SearchBar'
-import axios from 'axios'
-import { key } from './unsplashKey'
 
 export default {
   name: 'App',
   data(){
     return{
-      myTest: [],
-      term: ''
-
+     
     }
   },
   components: {
     SearchBar
   },
-  methods: {
-    
-   async fetchSearchTermPhotos(){
-      const {data} = await axios.get('https://api.unsplash.com/search/photos?', {
-        params:{
-          query: 'london',
-          client_id: key
-        }
-      });
-      this.myTest = data.results;
-    },
 
-
-    async fetchRandomPhotos(){
-      const {data} = await axios.get('https://api.unsplash.com/photos', {
-        params:{
-          client_id: key,
-          per_page: 12
-        }
-      });
-      this.myTest = data;
-    },
-
-    termSubmitted(term){
-      console.log(term)
-    }
-
-
-  },
-  mounted(){
-    //this.fetchSearchTermPhotos();
-    //this.fetchRandomPhotos();
-    console.log(this.myTest)
-  },
-  updated(){
-    console.log(this.myTest);
-  }
-
-  
    
 }
 </script>
@@ -68,6 +25,7 @@ export default {
 
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -78,5 +36,6 @@ export default {
   margin-bottom: 60px;
   font-family: 'Arimo', sans-serif;
 }
+
 </style>
 
